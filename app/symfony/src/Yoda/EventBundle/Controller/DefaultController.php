05f0b9209d428aa23cc770161f3a701dc55a791d
Arian Priolo <arianpriolo@gmail.com>
2021-06-02 14:37:08 -0300

    completed entity manager event

--- app/symfony/src/Yoda/EventBundle/Controller/DefaultController.php
+++ app/symfony/src/Yoda/EventBundle/Controller/DefaultController.php
@@ -9,10 +9,18 @@ class DefaultController extends Controller
 {
     public function indexAction(String $name): Response
     {
+        // Get entity manager
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
+
+        $event = $repo->findOneBy([
+            'name' => 'Testting name'
+        ]);
+
         // return a json response
-//        return new Response("teste", 200, ["Content-Type" => 'application/json']);
+        // return new Response("teste", 200, ["Content-Type" => 'application/json']);
         return $this->render('EventBundle:Default:index.html.twig',[
-            'name' => $name
+            'event' => $event
         ]);
     }
 }
